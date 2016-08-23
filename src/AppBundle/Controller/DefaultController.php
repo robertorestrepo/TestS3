a9dbd09231a6914a159db0459a5ad4f790cb1ba4
Roberto Alejandro Restrepo Rivera <roberto.restrepo@imexhs.com>
2016-08-23 12:39:10 -0500

    Se crean vistas iniciales del CRUD

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,11 +11,33 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request){
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render('default/index.html.twig');
     }
+	
+	/**
+     * @Route("/create", name="Registrar Empleado")
+     */
+    public function craeteAction(Request $request){
+        // replace this example code with whatever you need
+        return $this->render('crud/create.html.twig');
+    }
+	
+	/**
+     * @Route("/list", name="Listar_Empleados")
+     */
+    public function listAction(){
+        // replace this example code with whatever you need
+        return $this->render('crud/list.html.twig');
+    }
+	
+	/**
+     * @Route("/edit/{id}", name="Editar_Empleado")
+     */
+    public function editAction($id, Request $request){
+        // replace this example code with whatever you need
+        return $this->render('crud/edit.html.twig');
+    }
+	
 }
