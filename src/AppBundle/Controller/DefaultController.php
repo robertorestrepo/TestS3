bafbbb63c652fc408c42a1e3e4428f1061e61bf4
Roberto Alejandro Restrepo Rivera <roberto.restrepo@imexhs.com>
2016-09-05 16:49:06 -0500

    Adelantos Formularios

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,12 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use AppBundle\Entity\User;
 
 class DefaultController extends Controller
 {
@@ -13,8 +19,9 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request){
         // replace this example code with whatever you need
-        $csrfToken = $this->get('security.csrf.token_manager')->isGranted('ROLE_ADMIN');
-        return $this->render('default/index.html.twig', array('ROLE' => $csrfToken ));
+//        $csrfToken = $this->get('security.csrf.token_manager')->isGranted('ROLE_ADMIN');
+//        return $this->render('default/index.html.twig', array('ROLE' => $csrfToken ));
+        return $this->render('default/index.html.twig');
     }
 	
 	/**
@@ -22,7 +29,37 @@ class DefaultController extends Controller
      */
     public function craeteAction(Request $request){
         // replace this example code with whatever you need
-        return $this->render('crud/create.html.twig');
+        //return $this->render('crud/create.html.twig');
+        
+        $usuario = new User();
+        
+        $form = $this->createFormBuilder($usuario)
+                ->add('email', EmailType::class, array('label' => 'Correo Electronico'))
+                ->add('username', TextType::class, array('label' => 'Usuario'))
+                ->add('plainPassword', RepeatedType::class , array(
+                'type' => PasswordType::class ,
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'form.password'),
+                'second_options' => array('label' => 'form.password_confirmation'),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ))
+                ->add('save', SubmitType::class, array('label' => 'Create Task'))
+                ->getForm();
+        
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($usuario);
+            $em->flush();
+            return $this->redirectToRoute('task_success');
+            
+        }
+        
+        return $this->render('crud/create.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 	
 	/**
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,249 +1,32 @@
 <?php
-
+// src/AppBundle/Entity/User.php
 namespace AppBundle\Entity;
-
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
-
 /**
- * UserApp
- *
- * @ORM\Entity(repositoryClass="AppBundle\Repository\UserAppRepository")
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
  */
-class User implements UserInterface, \Serializable
+class User extends BaseUser
 {
     /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
+     * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
     
     /**
      * @var string
      *
-     * @ORM\Column(name="empresa", type="string", length=255)
+     * @ORM\Column(name="empresa", type="string", length=255, nullable=true)
      */
     private $empresa;
     
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="direccion", type="string", length=255)
-     */
-    private $direccion;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="username", type="string", length=255, unique=true)
-     */
-    private $username;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="password", type="string", length=255)
-     */
-    private $password;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="email", type="string", length=255)
-     */
-    private $email;
-
-    /**
-     * @var bool
-     *
-     * @ORM\Column(name="enabled", type="boolean")
-     */
-    private $enabled;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="roles", type="text")
-     */
-    private $roles;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="salt", type="string", length=255)
-     */
-    private $salt;
-
-    /**
-     * @var bool
-     *
-     * @ORM\Column(name="credentials_expired", type="boolean")
-     */
-    private $credentialsExpired;
-    
     public function __construct()
     {
-        $this->isActive = true;
-        // may not be needed, see section on salt below
-        // $this->salt = md5(uniqid(null, true));
-    }
-
-
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set username
-     *
-     * @param string $username
-     *
-     * @return UserApp
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
-    
-        return $this;
-    }
-
-    /**
-     * Get username
-     *
-     * @return string
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    /**
-     * Set password
-     *
-     * @param string $password
-     *
-     * @return UserApp
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-    
-        return $this;
-    }
-
-    /**
-     * Get password
-     *
-     * @return string
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    /**
-     * Set email
-     *
-     * @param string $email
-     *
-     * @return UserApp
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    
-        return $this;
-    }
-
-    /**
-     * Get email
-     *
-     * @return string
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * Set isActive
-     *
-     * @param boolean $isActive
-     *
-     * @return UserApp
-     */
-    public function setEnabled($enabled)
-    {
-        $this->enabled = $enabled;
-    
-        return $this;
-    }
-
-    /**
-     * Get isActive
-     *
-     * @return boolean
-     */
-    public function getEnabled()
-    {
-        return $this->enabled;
-    }
-
-    /**
-     * Set roles
-     *
-     * @param string $roles
-     *
-     * @return UserApp
-     */
-    public function setRoles($roles)
-    {
-        $this->roles = $roles;
-    
-        return $this;
-    }
-
-    /**
-     * Get roles
-     *
-     * @return string
-     */
-    public function getRoles()
-    {
-        return $this->roles;
-    }
-
-    /**
-     * Set salt
-     *
-     * @param string $salt
-     *
-     * @return UserApp
-     */
-    public function setSalt($salt)
-    {
-        $this->salt = $salt;
-    
-        return $this;
-    }
-
-    /**
-     * Get salt
-     *
-     * @return string
-     */
-    public function getSalt()
-    {
-        return $this->salt;
+        parent::__construct();
+        // your own logic
     }
     
     /**
@@ -259,7 +42,6 @@ class User implements UserInterface, \Serializable
     
         return $this;
     }
-
     /**
      * Get empresa
      *
@@ -269,81 +51,4 @@ class User implements UserInterface, \Serializable
     {
         return $this->empresa;
     }
-    
-    /**
-     * Set empresa
-     *
-     * @param string $empresa
-     *
-     * @return UserApp
-     */
-    public function setDireccion($direccion)
-    {
-        $this->direccion = $direccion;
-    
-        return $this;
-    }
-
-    /**
-     * Get empresa
-     *
-     * @return string
-     */
-    public function getDireccion()
-    {
-        return $this->direccion;
-    }
-
-    /**
-     * Set credentialsExpired
-     *
-     * @param boolean $credentialsExpired
-     *
-     * @return UserApp
-     */
-    public function setCredentialsExpired($credentialsExpired)
-    {
-        $this->credentialsExpired = $credentialsExpired;
-    
-        return $this;
-    }
-
-    /**
-     * Get credentialsExpired
-     *
-     * @return boolean
-     */
-    public function getCredentialsExpired()
-    {
-        return $this->credentialsExpired;
-    }
-    
-    /** @see \Serializable::serialize() */
-    public function serialize()
-    {
-        return serialize(array(
-            $this->id,
-            $this->username,
-            $this->password,
-            // see section on salt below
-            // $this->salt,
-        ));
-    }
-    
-    /** @see \Serializable::unserialize() */
-    public function unserialize($serialized)
-    {
-        list (
-            $this->id,
-            $this->username,
-            $this->password,
-            // see section on salt below
-            // $this->salt
-        ) = unserialize($serialized);
-    }
-    
-    public function eraseCredentials()
-    {
-    }
 }
-
